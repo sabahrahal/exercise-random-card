baf6ed3aa37c24030e0edace21af20daeba04c45
Sabah Rahal <sabahrahal1998@gmail.com>
2022-09-12 04:38:06 +0000

    Random Card Solution

--- src/app.js
+++ src/app.js
@@ -7,5 +7,52 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const pintas = ["♥", "♦", "♠", "♣"];
+
+  let textNumberHtml = document.querySelector(".number-card");
+  let pintaTop = document.querySelector("#pinta-top");
+  let pintaBottom = document.querySelector("#pinta-bottom");
+
+  function randomCard(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  function card() {
+    let pinta = pintas[randomCard(pintas)];
+    let number = numbers[randomCard(numbers)];
+
+    if (pinta === "♠" || pinta === "♣") {
+      pintaTop.innerHTML = pinta;
+      pintaBottom.innerHTML = pinta;
+      textNumberHtml.innerHTML = number;
+
+      pintaTop.style.color = "black";
+      pintaBottom.style.color = "black";
+    } else {
+      pintaTop.innerHTML = pinta;
+      pintaBottom.innerHTML = pinta;
+      textNumberHtml.innerHTML = number;
+
+      pintaTop.style.color = "red";
+      pintaBottom.style.color = "red";
+    }
+  }
+
+  card();
 };
